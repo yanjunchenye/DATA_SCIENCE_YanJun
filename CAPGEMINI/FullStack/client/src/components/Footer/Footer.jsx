f947eae9b40d12600ec0f20acf6f3bf096a95b29
littlexen69 <yanjunchen69@gmail.com>
2025-10-10 11:25:08 +0200

    ok

--- CAPGEMINI/FullStack/client/src/components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  return <footer className="footer-dashboard">FireWatch © 2025 v1.0</footer>;
-};
-
-export default Footer;
--- CAPGEMINI/FullStack/client/src/context/UserContext/useUser.js
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { UserContext } from "./UserContextContext";
-
-export const useUser = () => useContext(UserContext);
